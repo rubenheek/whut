e86f379306f03a8c5aaf0a9c0417d1d955461ae2
Ruben <rubenheek@icloud.com>
2016-11-08 20:13:49 +0100

    array query funtion with promise

--- main.js
+++ main.js
@@ -4,5 +4,24 @@ var config = {
     databaseURL: "https://whut-9f376.firebaseio.com"
     }
 var app = firebase.initializeApp(config);
+var db = app.database();
 
-app.database().ref("test").set(["a", "b", "c"]);
+var q = document.getElementById('questions');
+
+db.ref("test").on("child_added", function(data) {
+    console.log(data.val());
+    q.innerHTML += "<p>" + data.val() + "</p>";
+    console.log(q);
+});
+
+function getArrayData(ref, type) {
+    return db.ref(ref).once(type).then(function(d) {
+        return d.val();
+    });
+}
+
+var ad = getArrayData("test", "value").then(function(d) {
+    ad = d;
+    console.log(ad);
+});
+console.log(ad);
