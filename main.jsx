1ed4726926391cc7c219d80a2c179c87a9975847
rubenheek <rubenheek@icloud.com>
2016-12-06 21:12:18 +0100

    node modules, removed unused code

--- main.jsx
+++ main.jsx
@@ -1,3 +1,9 @@
+// import React from 'react';
+// import reactDOM from 'react-dom';
+// import firebase from 'firebase';
+// import reactfire from 'reactfire';
+// import {Router, Route, Link, hashHistory} from 'react-router';
+
 const config = {
         authDomain: "whut-9f376.firebaseapp.com",
         databaseURL: "https://whut-9f376.firebaseio.com"
@@ -5,70 +11,236 @@ const config = {
 let app = firebase.initializeApp(config);
 let db = app.database();
 
-function getData(ref, type) {
-    return db.ref(ref).once(type).then((d) => {
-        return d.val();
-    });
-}
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var Link = ReactRouter.Link;
+var hashHistory = ReactRouter.hashHistory;
 
-const QuestionForm = React.createClass({
-    mixins: [ReactFireMixin],
-    getInitialState() {
-        return {
-            text: ''
-        }
-    },
-    pushQuestion(e) {
-        e.preventDefault();
-        this.firebaseRefs['letters'].push(
-            this.state.text
-        );
-        this.setState({text: ""});
-    },
-    onChange(e) {
-        this.setState({text: e.target.value});
-    },
-    componentWillMount() {
-        let ref = firebase.database().ref("test");
-        this.bindAsArray(ref, "letters");
-    },
+const App = React.createClass({
     render() {
-        return (
-            <form onSubmit={this.pushQuestion}>
-             <input onChange={this.onChange} value={this.state.text}/>
-             <button>ask</button>
-            </form>
+        return(
+            <div>
+                <div className="toolbar">
+                    <span>?</span>
+                    <span>Whut - app2</span>
+                </div>
+                {this.props.children}
+            </div>
         )
     }
 });
 
-const Questions = React.createClass({
+const Default = React.createClass({
+    render() {
+        return(
+            <div>
+             <p>No match</p>
+             <Link to="/login">login</Link>
+            </div>
+        )
+    }
+});
+
+//login form
+const Login = React.createClass({
     mixins: [ReactFireMixin],
     getInitialState() {
         return {
-            letters: []
+            studentID: ""
+        }
+    },
+    addStudent(e) {
+        e.preventDefault();
+        let studentID = this.state.studentID;
+        let ref = firebase.database().ref('students');
+        ref.once('value', (snapshot) => {
+            if(snapshot.hasChild(studentID)) {
+                console.log(studentID +  ' exists');
+                hashHistory.push('/student/' + studentID);
+            } else {
+                console.log('adding ' + studentID);
+                this.setState({studentID: ""});
+                this.firebaseRefs['students'].child(studentID).set({
+                    'name': '',
+                    'lessons': []
+                }, (callback) => {
+                    console.log(callback);
+                    hashHistory.push('/student/' + studentID);
+                });
+            }
+        }); 
+    },
+    handleChange(e) {
+        this.setState({studentID: e.target.value});
+    },
+    componentWillMount() {
+        let ref = firebase.database().ref('students');
+        this.bindAsObject(ref, 'students');
+        //remove bind, use at addStudent() instead 
+    },
+    teacherLogin() {
+        let teacherID = 'grj';
+        hashHistory.push('/teacher/' + teacherID);
+    },
+    render() {
+        return (
+            <div>
+             <form onSubmit={this.addStudent}>
+              <input type="text" value={this.state.studentID} placeholder="leerlingnummer" onChange={this.handleChange}/>
+              <input type="submit" value="Login"/>
+             </form>
+             <button onClick={this.teacherLogin}>Wacht ff, ik ben een docent</button>
+            </div>
+        )
+    }
+});
+
+//overview for student with groups he added
+const StudentOverview = React.createClass({
+    mixins: [ReactFireMixin],
+    getInitialState() {
+        return {
+            groups: [],
+            name: {}
         }
     },
     componentWillMount() {
-        var ref = firebase.database().ref("test");
-        this.bindAsArray(ref, "letters");
-    },
-    deleteQuestion(key) {
-        console.log(key);
-        this.firebaseRefs['letters'].child(key).remove();
+        let groupsRef = firebase.database().ref('students/' + this.props.params.studentID + '/groups');
+        this.bindAsArray(groupsRef, 'groups');
+        let nameRef = firebase.database().ref('students/' + this.props.params.studentID + '/name');
+        this.bindAsObject(nameRef, 'name');
     },
     render() {
-        return(
-            <ul>
-                {this.state.letters.map((letter, index) => {
-                    return (
-                        <li>
-                         <span key={index}>{letter['.value']}</span>
-                         <button onClick={this.deleteQuestion.bind(this, index)}>delete</button>
-                        </li>
-                    )
-                })}
-            </ul>
-        );
+        let noGroups;
+        if(!this.state.groups.length) {
+            noGroups = (<li>Not enrolled in any groups yet</li>);
+        }
+        return ( 
+            <div>
+                <p>Welkom {this.state.name['.value'] || this.props.params.studentID}</p>
+                <span>Groups</span>
+                <ul>
+                    {noGroups}
+                    {this.state.groups.map((group, index) => {
+                        return (
+                            <li key={index}>
+                                <Link to={"student/" + this.props.params.studentID + "/group/" + index}>{group.name}</Link>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+        )
     }
 });
+
+//question form
+const GroupForm = React.createClass({
+    mixins: [ReactFireMixin],
+    getInitialState() {
+        return {
+            group: {
+                questions: []
+            },
+            questions: [],
+            questionInput: ""
+        }
+    },
+    componentWillMount() {
+        let groupRef = firebase.database().ref('groups/' + this.props.params.groupID);
+        this.bindAsObject(groupRef, 'group');
+        let questionsRef = firebase.database().ref('groups/' + this.props.params.groupID + '/questions');
+        this.bindAsArray(questionsRef, 'questions');
+    },
+    handleChange(e) {
+        this.setState({questionInput: e.target.value});
+    },
+    pushQuestion(e) {
+        e.preventDefault();
+        console.log(this.state.questionInput);
+        if(!this.state.questionInput) {
+            return;
+        }
+        this.firebaseRefs['group'].child('questions').push({
+            student: this.props.params.studentID,
+            text: this.state.questionInput
+        });
+        this.setState({questionInput: ""});
+    },
+    render() {
+        let noQuestions;
+        if(!this.state.questions) {
+            noQuestions = (<li>No questions asked yet</li>);
+        }
+        return (
+            <div>
+                <h3>{this.state.group.name}</h3>
+                <span>Questions</span>
+                <lu>
+                    {noQuestions}
+                    {this.state.questions.map((question, index) => {
+                        return (
+                            <li key={index}>{question.text}</li>
+                        )
+                    })}
+                </lu>
+                <br/>
+                <form onSubmit={this.pushQuestion}>
+                    <input type="text" value={this.state.questionInput} placeholder="question" onChange={this.handleChange}/>
+                    <input type="submit" value="Ask"/>
+                </form>
+            </div>
+        )
+    }
+});
+
+const TeachterOverview = React.createClass({
+    mixins: [ReactFireMixin],
+    checkID(id) {
+        let groupsRef = firebase.database().ref('groups');
+        groupsRef.once('value', (snapshot) => {
+            if(snapshot.hasChild(id)) {
+                this.checkID(this.generateID());
+            } else {
+                console.log('adding ' + id);
+            }
+            return;
+        });
+    },
+    generateID() {
+        let id = '';
+        for(let i=0; i<7; i++) {
+            id += Math.floor(Math.random()*10).toString();
+        }
+        return id;
+    },
+    createGroup() {
+        let groupID = this.checkID(this.generateID());
+        console.log(groupID);
+        firebase.database().ref('groups').child(groupID).set({
+            'owner': this.props.params.teacherID
+        }, (callback) => {
+            console.log(callback);
+            hashHistory.push('/teacher/' + groupID);
+        });
+        return;
+    },
+    render() {
+        return (
+            <button onClick={this.createGroup}>Maak klas aan</button>
+        )
+    }
+});
+
+//routing
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <Route path="login" component={Login}/>
+            <Route path="student/:studentID" component={StudentOverview}/>
+            <Route path="student/:studentID/group/:groupID" component={GroupForm}/>
+            <Route path="teacher/:teacherID" component={TeachterOverview}/>
+        </Route>
+        <Route path="*" component={Default}/>
+    </Router>,
+    document.getElementById('content'));
